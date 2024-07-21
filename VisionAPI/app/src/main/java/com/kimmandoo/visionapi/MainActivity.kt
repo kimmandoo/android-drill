5c32021408cf36566edb00b2d4ba3f3eb40cc061
kimmandoo <mingyu5675@gmail.com>
2024-07-21 17:09:54 +0900

    feat: Vision API 호출하는 비용 최소화

--- VisionAPI/app/src/main/java/com/kimmandoo/visionapi/MainActivity.kt
+++ VisionAPI/app/src/main/java/com/kimmandoo/visionapi/MainActivity.kt
@@ -96,26 +96,4 @@ class MainActivity : AppCompatActivity() {
             null
         }
     }
-
-    private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
-        val (height: Int, width: Int) = options.run { outHeight to outWidth }
-        var inSampleSize = 1
-
-        if (height > reqHeight || width > reqWidth) {
-            val halfHeight: Int = height / 2
-            val halfWidth: Int = width / 2
-
-            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
-                inSampleSize *= 2
-            }
-        }
-
-        return inSampleSize
-    }
-
-    private fun removeExifData(uri: Uri): Bitmap? {
-        val inputStream = contentResolver.openInputStream(uri)
-        val options = BitmapFactory.Options().apply { inJustDecodeBounds = false }
-        return BitmapFactory.decodeStream(inputStream, null, options)
-    }
 }
