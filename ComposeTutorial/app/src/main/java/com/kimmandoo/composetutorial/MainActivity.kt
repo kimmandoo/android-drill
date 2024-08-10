5c3969cee8de179fc992e57220f274fc67e68530
kimmandoo <mingyu5675@gmail.com>
2024-08-10 18:02:42 +0900

    feat: 컴포즈 시작

---
+++ ComposeTutorial/app/src/main/java/com/kimmandoo/composetutorial/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.kimmandoo.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kimmandoo.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeTutorialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TestBox() {
+    var isSelected by remember { mutableStateOf(false) }
+
+    Column {
+        Box(
+            modifier = Modifier
+                .size(200.dp)
+                .padding(40.dp)
+                .background(Color.Red)
+        ) {
+        }
+        Box(modifier = Modifier.size(0.dp, 20.dp))
+        Box(
+            modifier = Modifier.padding(40.dp)
+                .size(200.dp)
+                .then(
+                    if (isSelected) Modifier
+                        .background(Color.Blue)
+                    else Modifier
+                        .background(Color.Red)
+                )
+        )
+        Button(onClick = { isSelected = !isSelected }) {
+            Text(if (isSelected) "선택됨" else "선택 안됨")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserProfile() {
+    Column {
+        Text("유저")
+        Text("abcd@a.b")
+        var likes by remember { mutableStateOf(0) }
+        Text("Likes: $likes")
+        Button(onClick = { likes++ }) {
+            Text("Like")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTutorialTheme {
+        Greeting("Android")
+    }
+}
