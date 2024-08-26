ac30658569a1426a8e1768791379e20d04320cb4
Mingyu Kim <mingyu5675@gmail.com>
2024-08-26 11:01:00 +0900

    feat: fashmesh 초기 세팅

--- FaceMesh/app/build.gradle.kts
+++ FaceMesh/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
     implementation(libs.face.mesh.detection)
