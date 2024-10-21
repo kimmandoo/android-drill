56cc276a90d53ba7407bc6e90c054234669ad56d
김민규 <mingyu5675@gmail.com>
2024-10-21 22:52:44 +0900

    feat: 직렬화 조심

--- MVIPractice/data/src/main/java/com/kimmandoo/data/model/UserResponse.kt
+++ MVIPractice/data/src/main/java/com/kimmandoo/data/model/UserResponse.kt
@@ -1,14 +1,20 @@
 package com.kimmandoo.data.model
 
 import com.kimmandoo.domain.model.User
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserResponse(
+    @SerialName("id")
     val id: Long,
+    @SerialName("loginId")
     val loginId: String,
+    @SerialName("userName")
     val username: String,
+    @SerialName("extraUserInfo")
     val extraUserInfo: String,
+    @SerialName("profileFilePath")
     val profileFilePath: String,
 )
 
